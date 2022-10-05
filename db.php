7566674e9df6f8281e7d26676f5b1916c9d529a0
DennisFernandes90 <dennisfernandes2012@hotmail.com>
2022-10-05 11:24:32 -0300

    Inclusa a senha no arquivo db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     $db_name = "comentarios_ajax";
     $db_host = "localhost";
     $db_user = "root";
-    $db_pass = "";
+    $db_pass = "mysql";
 
     $conn = new PDO("mysql:dbname=". $db_name . ";host=" . $db_host, $db_user, $db_pass);
 
